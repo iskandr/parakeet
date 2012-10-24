85a1e6b75a2f597468cebab3f71ca534b1f2b9b6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-10-24 17:34:24 -0400

    added simplification optimization

---
+++ simplify.py
@@ -0,0 +1,27 @@
+import transform
+import syntax
+import prims 
+from syntax_helpers import collect_constants, is_one, is_zero, all_constants 
+import syntax 
+
+class Simplify(transform.Transform):
+  def transform_PrimCall(self, expr):
+    args = self.transform_expr_list(expr.args)
+    prim = expr.prim  
+    if all_constants(args):
+      return syntax.Const(value = prim.fn(*collect_constants(args)), type = expr.type)
+    elif prim == prims.add:
+      if is_zero(args[0]):
+        return args[1]
+      elif is_zero(args[1]):
+        return args[0]   
+    elif prim == prims.multiply:
+      if is_one(args[0]):
+        return args[1]
+      elif is_one(args[1]):
+        return args[0]
+      elif is_zero(args[0])  or is_zero(args[1]):
+        return syntax.Const(value = 0, type = expr.type)
+    elif prim == prims.divide and is_one(args[1]):
+      return args[0]
+    return syntax.PrimCall(prim = prim, args = args, type = expr.type) 
