4e2d0fbdce18bc2224ef790a1ccffef3d07275b1
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-11-10 16:23:19 -0500

    added syntax visitor to abstract out shape inference's traversal

--- shape_inference.py
+++ shape_inference.py
@@ -1,74 +1,9 @@
-
-from args import match
-
-class SyntaxVisitor(object):  
-  def visit_generic_expr(self):
-    pass
-  
-  def visit_expr(self, expr):
-    method_name = 'vist_' + expr.node_type()
-    if hasattr(self, method_name):
-      method = getattr(self, method_name)
-      return method(expr)
-    else:
-      return self.visit_generic_expr(expr)
-  
-  def visit_expr_list(self, exprs):
-    return [self.visit_expr(e) for e in exprs]
-  
-  def visit_expr_tuple(self, exprs):
-    return tuple(self.visit_expr_list(exprs))
-  
-  def visit_generic_stmt(self):
-    pass
-      
-  def visit_stmt(self, stmt):
-    method_name = 'vist_' + stmt.node_type()
-    if hasattr(self, method_name):
-      method = getattr(self, method_name)
-      return method(stmt)
-    else:
-      return self.visit_generic_stmt(stmt) 
-
+import syntax
+from syntax_visitor import SyntaxVisitor 
 from node import Node  
 
-class IncompatibleShapes(Exception):
-  def __init__(self, s1, s2):
-    self.s1 = s1
-    self.s2 = s2 
-    
-  def __str__(self):
-    return "IncompatibleShapes(%s, %s)" % (self.s1, self.s2)
-  
-  def __repr__(self):
-    return str(self)
-
-
-class ShapeElt(Node):
-  pass 
-
-class Const(ShapeElt):
-  def __init__(self, value):
-    self.value = value 
-    
-  def __eq__(self, other):
-    return isinstance(other, Const) and \
-      self.value == other.value 
-      
-def is_zero(d):
-  return isinstance(d, Const) and d.value == 0
-
-def is_one(d):
-  return isinstance(d, Const) and d.value == 1
-
-class DimSize(ShapeElt):
-  def __init__(self, var, dim):
-    self.var = var
-    self.dim = dim 
-    
-  def __eq__(self, other):
-    return self.var == other.var and self.dim == other.dim 
-
+   
+"""
 class Max(ShapeElt):
   def __init__(self, d1, d2):
     self.d1 = d1
@@ -93,100 +28,182 @@ def max_dim(d1, d2):
       return d1 
   else:
     return Max(d1, d2)
-  
-class SymbolicShape(Node):
-  def __eq__(self, other):
-    return False
-  
-  def combine_eq(self, other):
-    if self == other:
-      return self
-    else:
-      raise IncompatibleShapes(self, other)
- 
-class UnknownShape(SymbolicShape):
-  def combine_eq(self, other):
-    return other
-  
-  def combine_max(self, other):
-    return other 
-    
+"""
 
-class ArrayShape(SymbolicShape):
+
+
+class ValueMismatch(Exception):
+  """
+  Raise this exception whenever two incompatible
+  abstract values get combined
+  """
+  def __init__(self, v1, v2):
+    self.v1 = v1
+    self.v2 = v2 
+
+  def __str__(self):
+    return "ValueMismatch(%s, %s)" % (self.v1, self.v2)
+
+  def __repr__(self):
+    return str(self)
+  
+  
+class AbstractValue(Node):
+  pass 
+
+
+class ConstValue(AbstractValue):
+  def __init__(self, value):
+    self.value = value 
+
+def is_zero(d):
+  return isinstance(d, ConstValue) and d.value == 0
+
+def is_one(d):
+  return isinstance(d, ConstValue) and d.value == 1
+
+      
+class ScalarVar(AbstractValue):
+  def __init__(self, name):
+    self.name = name 
+
+class DimSize(AbstractValue):
+  """
+  The value producted by x.shape[d]
+  """
+  def __init__(self, var, dim):
+    self.var = var
+    self.dim = dim 
+    
+  def __eq__(self, other):
+    return isinstance(other, DimSize) and \
+      self.var == other.var and self.dim == other.dim
+  
+  def combine(self, other):
+    if self == other:
+      return self 
+    else:
+      raise ValueMismatch(self, other)
+
+class ArrayValue(AbstractValue):
   def __init__(self, dims):
     self.dims = dims 
-    
+    self.rank = len(dims)
+      
   def __eq__(self, other):
-    return isinstance(other, ArrayShape) and \
+    return isinstance(other, ArrayValue) and \
       len(self.dims) == len(other.dims) and \
       all(d1 == d2 for (d1,d2) in zip(self.dims, other.dims) )
   
-  def combine_max(self, other):
-    if isinstance(other, ArrayShape):
-      my_rank = len(self.dims)
-      other_rank = len(other.dims)
-      if my_rank == 0:
-        return other
-      elif other_rank == 0:
+  def combine(self, other):
+    if isinstance(other, ArrayValue):
+      if is_scalar(other):
         return self
-      elif my_rank == other_rank:
-        return [max_dim(d1, d2) for (d1, d2) 
-                in zip(self.arg_shapes, other.arg_shapes)]       
-    raise IncompatibleShapes()
+      elif is_scalar(self):
+        return other
+      elif other.rank == self.rank:
+        
+        assert False, "Shouldn't the logic of unifying dim variables be in the interpreter?"
+    raise ValueMismatch(self, other)
+  
+# represent scalars with 0-rank array s
+unknown_scalar = ArrayValue(())
+    
+def is_scalar(v):
+  return isinstance(v, (ConstValue, ScalarVar, DimSize)) or \
+    (isinstance(v, ArrayValue) and v.rank == 0)
 
-class ClosureShape(SymbolicShape):
-  def __init__(self, untyped_fn, arg_shapes):
-    self.untyped_fn = untyped_fn 
-    self.arg_shapes = arg_shapes 
+class SliceValue(AbstractValue):
+  def __init__(self, start, stop, step):
+    self.start = start
+    self.stop = stop 
+    self.step = step 
     
   def __eq__(self, other):
-    return isinstance(other, ClosureShape) and \
+    return isinstance(other, SliceValue) and \
+      self.start == other.start and \
+      self.stop == other.stop and \
+      self.step == other.step 
+  
+  def combine(self, other):
+    if isinstance(other, SliceValue):
+      start = self.start.combine(other.start)
+      stop = self.stop.combine(other.stop)
+      step = self.step.combine(other.step)
+      return SliceValue(start, stop, step)
+    else:
+      raise ValueMismatch(self, other)
+
+def combine_value_lists(xs, ys):
+  return [xi.combine(yi) for (xi, yi) in zip(xs, ys)]
+
+def TupleValue(AbstractValue):
+  def __init__(self, elts):
+    self.elts = elts 
+    
+  def __eq__(self, other):
+    return isinstance(other, TupleValue) and \
+      len(self.elts) == len(other.elts) and \
+      all(e1 == e2 for (e1, e2) in zip(self.elts, other.elts))
+  
+  def __str__(self):
+    return "Tuple(%s)" % ", ".join(str(e) for e in self.elts)
+  
+  def combine(self, other):
+    if isinstance(other, TupleValue):
+      if len(self.elts) == len(other.elts):
+        return TupleValue(combine_value_lists(self.elts, other.elts))
+    raise ValueMismatch(self, other)
+  
+class ClosureValue(AbstractValue):
+  def __init__(self, untyped_fn, args):
+    self.untyped_fn = untyped_fn 
+    self.args = args 
+  
+  def __str__(self):
+    return "Closure(fn = %s, %s)" % \
+      (self.untyped_fn, ", ".join(str(e) for e in self.elts))
+  
+  def __eq__(self, other):
+    return isinstance(other, ClosureValue) and \
       self.untyped_fn == other.untyped_fn and \
       len(self.arg_shapes) == len(other.arg_shapes) and \
-      all(s1 == s2 for (s1,s2) in zip(self.arg_shapes, other.arg_shapes))
-
-  def combine_max(self, other):
-    if isinstance(other, ClosureShape) and \
-      self.untyped_fn == other.untyped_fn and \
-      len(self.arg_shapes) == len(other.arg_shapes):
-        max_arg_shapes = \
-          [s1.combine_max(s2) for (s1,s2) in
-           zip(self.arg_shapes, other.arg_shapes)]
-        return ClosureShape(self.untyped_fn, max_arg_shapes)
-    raise IncompatibleShapes(self, other)
-
-
-scalar_shape = ArrayShape(())
-unknown_shape = UnknownShape()
-
-def rank(self, s):
-  assert isinstance(s, ArrayShape)
-  return len(s.dims)
+      all(v1 == v2 for (v1,v2) in zip(self.args, other.args))
   
-def is_scalar(self, s):
-  return isinstance(s, ArrayShape) and rank(s) == 0 
+  def combine(self, other):
+    if isinstance(other, ClosureValue):
+      # TODO: Implement sets of closures like we have in the type system 
+      if self.untyped_fn == other.untyped_fn and \
+         len(self.args) == len(other.args) :
+        combined_args = combine_value_lists(self.args, other.args)
+        return ClosureValue(self.untyped_fn, combined_args)
+    raise ValueMismatch(self, other)  
 
-def max_shape(shapes):
-  s_max = unknown_shape
-  for s in shapes:
-    s_max = s_max.combine(s)
-  return s_max 
-
-def max_dims(shapes, axis):
-  """
-  TODO: skip scalars, slice into each array shape along the given axis
-  """
-  pass 
 
 class ShapeInference(SyntaxVisitor):
-  def visit_Const(self, _):
-    return scalar_shape  
   
+  def __init__(self):
+    self.env = {}
+    
+  def visit_Const(self, expr):
+    return ConstValue(expr.value)
+  
+  def visit_Var(self, expr):
+    name = expr.name 
+    assert name in self.env, "Unknown variable %s" % name
+    return self.env[name]
+  
+  def visit_Tuple(self, expr):
+    return TupleValue(self.visit_expr_list(expr.elts))
+
   def visit_Map(self, expr):
     arg_shapes = self.visit_expr_list(expr.args)
   
+  def bind(self, lhs, rhs):
+    if isinstance(lhs, syntax.Tuple)
+  
   def visit_Assign(self, stmt):
-    shapes = self.visit_expr(stmt.rhs)
-    match(self.lhs, shapes) 
+    rhs = self.visit_expr(stmt.rhs)
+    self.bind(self.lhs, rhs) 
       
   
---
+++ syntax_visitor.py
@@ -0,0 +1,90 @@
+import syntax 
+
+class SyntaxVisitor(object):  
+  """
+  Traverse the statement structure of a syntax block,
+  optionally collecting values 
+  """
+  def visit_generic_expr(self, expr):
+    pass 
+
+  def visit_expr(self, expr):
+    method_name = 'vist_' + expr.node_type()
+    if hasattr(self, method_name):
+      method = getattr(self, method_name)
+      return method(expr)
+    else:
+      return self.visit_generic_expr(expr)
+  
+  def visit_expr_list(self, exprs):
+    return [self.visit_expr(e) for e in exprs]
+  
+  def visit_expr_tuple(self, exprs):
+    return tuple(self.visit_expr_list(exprs))
+  
+  def visit_lhs(self, lhs):
+    return self.visit_expr(lhs)
+  
+  def after_Assign(self, new_lhs, new_rhs):
+    """
+    Gets called after children are visited
+    """
+    pass
+  
+  def after_While(self, new_cond, new_body, new_merge):
+    pass
+  
+  def after_Return(self, new_value):
+    pass
+  
+  def after_If(self, new_cond, new_true, new_false, new_merge):
+    pass
+   
+  def after_block(self, stmts):
+    pass
+  
+  def visit_block(self, stmts):
+    # only try to collect the returned result 
+    # for each statement if the descendant class
+    # has define some meaningful operation that 
+    # receives all these statements 
+    if self.after_block.im_class != SyntaxVisitor: 
+      new_stmts = [self.visit_stmt(s) for s in stmts]
+      return self.after_block(new_stmts)
+    else:
+      for s in stmts:
+        self.visit_stmt(s)
+  
+  def visit_generic_stmt(self, stmt):
+    if isinstance(stmt, syntax.Assign):
+      lhs = self.visit_lhs(stmt.lhs)
+      rhs = self.visit_expr(stmt.rhs)
+      return self.after_Assign(lhs, rhs)
+    elif isinstance(stmt, syntax.If):
+      cond = self.visit_expr(stmt.cond)
+      true = self.visit_block(stmt.true)
+      false = self.visit_block(stmt.false)
+      merge = self.visit_merge(stmt.merge)
+      return self.after_If(cond, true, false, merge)
+    elif isinstance(stmt, syntax.Return):
+      value = self.visit_expr(stmt.value)
+      return self.after_Return(value)
+    else:
+      assert isinstance(stmt, syntax.While), \
+        "Unexpected statement: " + str(stmt)
+      cond = self.visit_expr(stmt.cond)
+      body = self.visit_block(stmt.body)
+      merge = self.visit_merge(stmt.merge)
+      return self.after_While(cond, body, merge)
+    
+    
+  def visit_stmt(self, stmt):
+    method_name = 'vist_' + stmt.node_type()
+    if hasattr(self, method_name):
+      method = getattr(self, method_name)
+      return method(stmt)
+    else:
+      return self.visit_generic_stmt(stmt) 
+
+  def visit_fn(self, fn):
+    return self.visit_block(fn.body)
