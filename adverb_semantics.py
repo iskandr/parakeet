2c85707b6e0ef944fbdd2a4ad54172bf5bfd5de6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-11-16 01:14:30 -0500

    got adverb semantics interpreter running, is this a misadventure in Haskell land?

--- adverb_semantics.py
+++ adverb_semantics.py
@@ -1,177 +1,209 @@
-
-"""
-Gosh, I really wish I had OCaml functors or Haskell's type classes
-so I could more cleanly parameterize all this code by abstract
-Elt, Value, Fn, etc... types
-"""  
+import numpy as np 
 
 
-class AbstractSemantics:
+class BaseSemantics:
+  """
+  Gosh, I really wish I had OCaml functors or Haskell's type classes
+  so I could more cleanly parameterize all this code by these abstract types:
+    Fn       -- representations of functions 
+                i.e. the interpreter domain might 
+                have a function "lambda x: x+1" whereas
+                in the type domain this function is represented
+                by "Int -> Int"  
+    Value    -- arrays, scalars, and tuples
+    DimSize  -- abstract representation of shape elements
+    
+  Also, for code generation of loops we need 
+    Idx          -- representation of loop indices (possibly same as DimSize?)
+    DelayedValue -- mapping from index to abstract value
+    Acc          -- object with 
+  """  
   
-  @classmethod 
-  def elt_repr(cls, value, axis):
-    """
-    Slice this value along the given axis
-    """ 
-    raise NotImplementedError
+  def size_along_axis(self, value, axis):
+    return value.shape[axis]
   
-  @classmethod 
-  def apply_repr(cls, fn, elts):
-    # returns a computation representing how
-    # get the result for each element 
-    raise NotImplementedError 
+  def rank(self, value):
+    return np.rank(value)
+    
+  def accumulator(self, v):
+    return [v]
   
-  @classmethod 
-  def size_along_axis(cls, value, axis):
-    raise NotImplementedError
+  def get_acc(self, acc):
+    return acc[0]
   
-  @classmethod 
-  def has_axis(cls, value, axis):
-    raise NotImplementedError
+  def set_acc(self, acc, v):
+    acc[0] = v 
   
-  @classmethod 
-  def collect(cls, elt_result, niters, axis):
-    raise NotImplementedError  
+  def const_int(self, x):
+    return int(x)
   
-  @classmethod 
-  def accumulator(cls, init, elts):
-    raise NotImplementedError
+  def add(self, x, y):
+    return x + y 
   
-  @classmethod 
-  def last_result(cls, elt_result, niters, axis):
-    raise NotImplementedError 
+  def sub(self, x, y):
+    return x - y 
   
-  @classmethod 
-  def adverb_prelude(cls, xs, axis):
+  def array(self, size, elt):
+    return np.array([elt] * size)
+   
+  def shift_array(self, arr, offset):
+    return arr[offset:]
+  
+  def index(self, arr, idx):
+    return arr[idx]
+  
+  def tuple(self, elts):
+    return tuple(elts)
+  
+  def setidx(self, arr, idx, v):
+    arr[idx] = v
+    
+  def loop(self, start_idx, stop_idx, body):
+    for i in xrange(start_idx, stop_idx):
+      body(i)
+  
+  def check_equal_sizes(self, sizes):
+    assert len(sizes) > 0
+    first = sizes[0]
+    assert all(sz == first for sz in sizes[1:])
+  
+  def slice_value(self, start, stop, step):
+    return slice(start, stop, step) 
+  
+  def apply(self, fn, args):
+    return fn(*args)
+  
+  def apply_to_delayed(self, fn, args, idx):
+    curr_args = [x(idx) for x in args]
+    return self.apply(fn, curr_args)
+  
+  none = None 
+  null_slice = slice(None, None, None)
+  def identity_function(self, x):
+    return x
+  def trivial_combiner(self, x, y):
+    return y 
+    
+class AdverbSemantics(BaseSemantics):
+  """
+  Describe the behavior of adverbs in terms of 
+  lower-level value and iteration constructs.
+  
+  To get something other than an unfathomably slow
+  interpreter, override all the methods of BaseSemantics
+  and make them work for some other domain (such as types, 
+  shapes, or compiled expressions)
+  """
+
+  def build_slice_indices(self, rank, axis, idx):
+    indices = []
+    for i in xrange(rank):
+      if i == axis:
+        indices.append(idx)
+      else:
+        indices.append(self.slice(self.none, self.none, self.const_int(1)))
+    return self.tuple(indices)
+  
+  def slice_along_axis(self, arr, axis, idx):
+    r = self.rank(arr)
+    index_tuple = self.build_slice_indices(r, axis, idx)
+    return self.index(arr, index_tuple)
+  
+  def delayed_elt(self, x, axis):
+    return lambda idx: self.slice_along_axis(x, axis, idx)
+  
+  def adverb_prelude(self, xs, axis):
     if not isinstance(xs, (list, tuple)):
       xs = [xs]
-    # exclude scalars
-    args_with_axis = [x for x in xs if cls.has_axis(x, axis)]
-    axis_sizes = [cls.size_along_axis(x, axis) for x in args_with_axis]
+    axis_sizes = [self.size_along_axis(x, axis) 
+                  for x in xs
+                  if self.rank(x) >= axis]
     assert len(axis_sizes) > 0
-    sz = axis_sizes[0]
+    
     # all arrays should agree in their dimensions along the 
     # axis we're iterating over 
-    assert all(other_size == sz for other_size in axis_sizes[1:])
-    
-    elts = [cls.elt_repr(x, axis) for x in xs]
-    return sz, elts
+    self.check_equal_sizes(axis_sizes)
+    elts = [self.delayed_elt(x, axis) for x in xs]
+    return axis_sizes[0], elts
   
-  @classmethod  
-  def eval_map(cls, f,  xs, axis):
-    """
-    Abstract specification of a map's behavior
-    which relies on a semantics object S to 
-    give specific/concrete meaning to this function
-    for particular input argument types. 
-    """
-    niters, elts = cls.adverb_prelude(xs, axis)
-    elt_result = cls.apply_repr(f, elts)
-    return cls.collect(elt_result, niters, axis)
+  def eval_map(self, f,  xs, axis):
+    return self.eval_scan(
+      mapper = f, 
+      combine = self.trivial_combiner,  
+      emit = self.identity_function,  
+      init = None,
+      inclusive = True,  
+      values = xs, 
+      axis = axis) 
   
-  @classmethod 
-  def eval_reduce(cls, local_f, combine, init, xs, axis):
-    niters, elts = cls.adverb_prelude(xs, axis)
-    elt_result = cls.apply_repr(local_f, elts)
-    acc = cls.accumulator(init)
-    acc_result = cls.accumulate(combine, [acc, elt_result], niters)
-    return acc_result[niters]
-    
-  #@classmethod 
-  #def eval_scan(cls, mapper, combine, init, xs, axis):
-  #  niters, elts = cls.adverb_prelude(xs, axis)
-  #  elt_result = cls.apply_repr(mapper, elts)
-  #  acc = cls.accumulator(init, elt_result)
-  #  return cls.collect(acc, niters, axis)
   
-  @classmethod
-  def eval_scan(cls, mapper, combine, collect, init, axis, xs):
-     
+  def eval_reduce(self, f, combine, init, xs, axis):
+    prefixes = self.eval_scan(
+      mapper = f, 
+      combine = combine, 
+      emit = self.identity_function,  
+      init = init, 
+      inclusive = False, 
+      values = xs, 
+      axis = axis )
+    return prefixes[-1]
   
-import numpy as np
- 
-class InterpSemantics(AbstractSemantics):
-  """
-  Instantiate the abstract abverb semantics with:
-     type value = np.ndarray or scalar
-     type dimsize = int 
-     type elt_computation = (idx -> value)
-  """
-  @classmethod 
-  def has_axis(cls, value, axis):
-    return np.rank(value) > axis
-
-  @classmethod 
-  def size_along_axis(cls, value, axis):
-    return value.shape[axis]
-
-  @classmethod 
-  def elt_repr(cls, value, axis):
-    r = np.rank(value)
-    if r < axis:
-      return lambda idx: value 
+  def eval_scan(self, mapper, combine, emit, init, inclusive, values, axis):
+    niters, elts = self.adverb_prelude(values, axis)
+    delayed_map_result = lambda idx: self.apply_to_delayed(mapper, elts, idx)
+    zero = self.const_int(0)
+    one = self.const_int(1)
+    if init is None:
+      init = delayed_map_result(0)
+      start_idx = one 
     else:
-      select_all = slice(None, None, None)
-      indices = [select_all] * r 
-      print indices, axis, value, r 
-      def delayed_slice(idx):
-        indices[axis] = idx
-        return value[tuple(indices)]
-      return delayed_slice
-  
-  @classmethod   
-  def apply_repr(cls, fn, elt_reprs):
-    return lambda idx: fn(*[e(idx) for e in elt_reprs]) 
-  
-
-  @classmethod
-  def accumulator(cls, combine, init, delayed_elt):
-    return lambda idx: return acc(idx)
-    def delayed(idx):
-      elt_value = delayed_elt(idx)
-      
-    return delayed
-  @classmethod 
-  def accumulate(cls, combine, init, map_repr, niters):
-    acc = init 
-    for idx in xrange(niters):
-      acc = combine(acc, map_repr(idx))
-    return acc 
-   
-  @classmethod 
-  def accumulate_and_collect(cls, f, init, elt_reprs, niters):
-    result = [None] * niters
-    acc = init 
-    for idx in xrange(niters):
-      acc = f(acc, *[e(idx) for e in elt_reprs])
-      result[idx] = acc
+      start_idx = zero
+    result_size = self.sub(niters, start_idx)
+    if inclusive:
+      result = self.array(self.add(result_size, one), init)  
+      # start_idx = self.add(start_idx, one)  
+    else:
+      result = self.array(result_size, init)
+ 
+    acc = self.accumulator(init)
+    emitted_elt_repr = lambda idx: self.apply(emit, [self.get_acc(acc)])
+    def loop_body(idx):
+      return [
+        self.set_acc(acc, 
+          self.apply(combine, [self.get_acc(acc), delayed_map_result(idx)])),
+        self.setidx(result, idx, emitted_elt_repr(idx))
+      ]
+    self.loop(start_idx, niters, loop_body)
     return result 
   
-  @classmethod
-  def collect(cls, delayed_fn, niters, axis):
-    elts = [delayed_fn(i) for i in xrange(niters)]
-    return np.array(elts)
 
 if __name__ == '__main__':
+  interp = AdverbSemantics()
   x = np.array([1,4,9])
-  y = InterpSemantics.eval_map(np.sqrt, [x], 0)
+  y = interp.eval_map(np.sqrt, [x], 0)
   print "Map Input", np.sqrt, x
   print "Output", y
   print "Expected output", np.sqrt(x)
   print
   
-  total = InterpSemantics.eval_reduce(lambda x: x, np.add,  0, [x], 0) 
+  total = interp.eval_reduce(lambda x: x, np.add,  0, [x], 0) 
   print "Reduce Input", np.add, 0, x
   print "Reduce output", total 
   print "Expected output", np.sum(x)
   print 
   
-  prefixes = InterpSemantics.eval_scan(
+  prefixes = interp.eval_scan(
     mapper = lambda x:x, 
     combine = np.add, 
+    emit = lambda x: x, 
     init = 0, 
+    inclusive = False, 
     values = [x], 
     axis = 0)
+  print "Scan Input", x
+  print "Scan output", prefixes
+  print "Expected Scan output", np.cumsum(x)
+  
   
   
   
