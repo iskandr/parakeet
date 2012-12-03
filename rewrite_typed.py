0ccfbbf6d172ad96b40672ae8febca3d632370e0
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-12-03 02:52:55 -0500

    added rewrite_typed

---
+++ rewrite_typed.py
@@ -0,0 +1,172 @@
+import core_types
+import array_type 
+import tuple_type  
+
+import syntax
+import syntax_helpers
+import closure_type 
+from syntax_helpers import get_types
+from transform import Transform
+import function_registry
+from args import ActualArgs
+
+class SimplifyInvoke(Transform):
+   
+  def transform_Invoke(self, expr):
+    new_closure = self.transform_expr(expr.closure)
+    closure_t = new_closure.type
+    if isinstance(closure_t, closure_type.ClosureT):
+      untyped_fn = closure_t.fn
+      if isinstance(untyped_fn, str):
+        untyped_fn = function_registry.untyped_functions[untyped_fn]
+      n_closure_args = len(closure_t.arg_types) 
+      closure_args = \
+        [self.closure_elt(new_closure, i) 
+         for i in xrange(n_closure_args)]
+    else:
+      assert isinstance(new_closure, syntax.Fn)
+      untyped_fn = new_closure
+      closure_args = []
+    old_args = expr.args
+    if isinstance(old_args, (list, tuple)):
+      old_args = ActualArgs(old_args)
+    old_args = old_args.prepend_positional(closure_args)
+    new_args = old_args.transform(self.transform_expr)
+    arg_types = new_args.transform(syntax_helpers.get_type)
+    import type_inference 
+    typed_fundef = type_inference.specialize(untyped_fn, arg_types)
+    
+    # Drop arguments that are assigned defaults, 
+    # since we're assuming those are set in the body 
+    # of the function 
+    linear_args, extra = untyped_fn.args.linearize_values(new_args, 
+                                                          tuple_elts_fn = self.tuple_elts, 
+                                                          keyword_fn = lambda k, v: None)
+    combined_args = [x for x in (linear_args + extra) if x] 
+    
+    return syntax.Call(typed_fundef.name, combined_args, type = expr.type)
+    
+
+class RewriteTyped(SimplifyInvoke):
+  def __init__(self, fn):
+    Transform.__init__(self, fn)
+    self.fn_return_type = self.fn.type_env["$return"]
+
+  def coerce_expr(self, expr, t):
+    assert t is not None
+    expr = self.transform_expr(expr)
+    if expr.type == t:
+      return expr
+    elif isinstance(expr, syntax.Tuple):
+      if not isinstance(t, tuple_type.TupleT) or \
+         len(expr.type.elt_types) != t.elt_types:
+        raise core_types.IncompatibleTypes(expr.type, t)
+      else:
+        new_elts = []
+        for elt, elt_t in zip(expr.elts, t.elt_types):
+          new_elts.append(self.coerce_expr(elt, elt_t))
+        return syntax.Tuple(new_elts, type = t)
+    else:
+      return self.cast(expr, t)
+
+  def transform_merge(self, merge):
+    new_merge = {}
+    for (var, (left, right)) in merge.iteritems():
+      t = self.type_env[var]
+      new_left = self.coerce_expr(left, t)
+      new_right = self.coerce_expr(right, t)
+      new_merge[var] = (new_left, new_right)
+    return new_merge
+
+  def transform_lhs(self, lhs):
+    if isinstance(lhs, syntax.Var):
+      t = self.lookup_type(lhs.name)
+      if t == lhs.type:
+        return lhs
+      else:
+        return syntax.Var(lhs.name, type = t)
+    elif isinstance(lhs, syntax.Tuple):
+      elts = map(self.transform_lhs, lhs.elts)
+      elt_types = get_types(elts)
+      if elt_types != lhs.type.elt_types:
+        return syntax.Tuple(elts, type = tuple_type.make_tuple_type(elt_types))
+      else:
+        return lhs
+    else:
+      return lhs
+
+  def transform_PrimCall(self, expr):
+      arg_types = get_types(expr.args)
+      upcast_types = expr.prim.expected_input_types(arg_types)
+      result_type = expr.prim.result_type(upcast_types)
+      upcast_args = [self.coerce_expr(x, t)
+                     for (x,t) in zip(expr.args, upcast_types)]
+      return syntax.PrimCall(expr.prim, upcast_args, type = result_type )
+
+  def transform_Array(self, expr):
+    array_t = expr.type
+    elt_t = array_t.elt_type
+    assert array_t.rank > 0
+    if array_t.rank == 1:
+      new_elts = [self.coerce_expr(elt, elt_t) for elt in expr.elts]
+      return syntax.Array(new_elts, type = array_t)
+    else:
+      # need to allocate an output array and copy the elements in
+      first_elt = self.assign_temp(expr.elts[0], "first_elt")
+      elt_dims = [self.shape(first_elt, i) for i in xrange(array_t.rank - 1)]
+      n = len(expr.elts)
+      outer_dim = syntax_helpers.const(n)
+      all_dims = (outer_dim,) + tuple(elt_dims)
+      array = self.alloc_array(elt_t, all_dims, "array_literal")
+      for i, elt in enumerate(expr.elts):
+        idx_expr = self.index(array, i, temp = False)
+        # transform indexing to make missing indices explicit
+        self.assign(idx_expr, expr.elts[i])
+      return array
+
+  def transform_Slice(self, expr):
+    # None step defaults to 1
+    if isinstance(expr.step.type, core_types.NoneT):
+      start_t = expr.start.type
+      stop_t = expr.stop.type
+      step = syntax_helpers.one_i64
+      step_t = step.type
+      slice_t = array_type.make_slice_type(start_t, stop_t, step_t)
+      return syntax.Slice(expr.start, expr.stop, step, type = slice_t)
+    else:
+      return expr
+
+  def transform_stmt(self, stmt):
+    if isinstance(stmt, syntax.Assign):
+      new_lhs = self.transform_lhs(stmt.lhs)
+      lhs_t = new_lhs.type
+      assert lhs_t is not None, "Expected a type for %s!" % stmt.lhs
+      new_rhs = self.coerce_expr(stmt.rhs, lhs_t)
+      assert new_rhs.type and isinstance(new_rhs.type, core_types.Type), \
+        "Expected type annotation on %s, but got %s" % (new_rhs, new_rhs.type)
+      return syntax.Assign(stmt.lhs, new_rhs)
+
+    elif isinstance(stmt, syntax.If):
+      new_cond = self.coerce_expr(stmt.cond, core_types.Bool)
+      new_true_block = self.transform_block(stmt.true)
+      new_false_block = self.transform_block(stmt.false)
+      new_merge = self.transform_merge(stmt.merge)
+      return syntax.If(new_cond, new_true_block, new_false_block, new_merge)
+
+    elif isinstance(stmt, syntax.Return):
+      new_value = self.coerce_expr(stmt.value, self.fn_return_type)
+      return syntax.Return(new_value)
+
+    elif isinstance(stmt, syntax.While):
+      new_cond = self.coerce_expr(stmt.cond, core_types.Bool)
+      new_body = self.transform_block(stmt.body)
+      # insert coercions for left-branch values into the current block before
+      # the while-loop and coercions for the right-branch to the end of the loop
+      # body
+      new_merge = self.transform_merge(stmt.merge)
+      return syntax.While(new_cond, new_body, new_merge)
+    else:
+      raise RuntimeError("Not implemented: %s" % stmt)
+
+def rewrite_typed(typed_fundef):
+  return RewriteTyped(typed_fundef).apply(copy=False)
