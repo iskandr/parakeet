9e1b121777beed901934610ef38c62742c90c90e
Eric Hielscher <hielscher@gmail.com>
2012-12-12 11:59:43 -0500

    Added test to demonstrate problem with non-axis-0 indexing.

--- test_tiling.py
+++ test_tiling.py
@@ -25,7 +25,7 @@ id_fn = syntax.TypedFn(
   type_env = {})
 
 x_array = np.arange(10, dtype = np.int64)
-x2_array = np.arange(100, dtype = np.int64).reshape(10,10)
+x2_array = np.arange(80, dtype = np.int64).reshape(10,8)
 x_array_t = array_type.make_array_type(core_types.Int64, 1)
 x_2_array_t = array_type.make_array_type(core_types.Int64, 2)
 
@@ -52,6 +52,31 @@ map2_fn = syntax.TypedFn(
   input_types = [x_2_array_t],
   body = [syntax.Return(adverbs.Map(map_fn, [syntax.Var("X", type=x_2_array_t)],
                                     0, type=x_2_array_t))],
+  return_type = x_2_array_t,
+  type_env = {"X":x_2_array_t})
+
+id_fn_3 = syntax.TypedFn(
+  name = "id_fn_3",
+  arg_names = ["x"],
+  input_types = [x_array_t],
+  body = [syntax.Return(syntax.Var("x", type=x_array_t))],
+  return_type = x_array_t,
+  type_env = {"x":x_array_t})
+
+axis_fn = syntax.TypedFn(
+  name = "axis_fn",
+  arg_names = ["X"],
+  input_types = [x_2_array_t],
+  body = [syntax.Return(adverbs.Map(id_fn_3, [syntax.Var("X", type=x_2_array_t)],
+                                    1, type=x_2_array_t))],
+  return_type = x_2_array_t,
+  type_env = {"X":x_2_array_t})
+
+axis_assign_fn = syntax.TypedFn(
+  name = "axis_assign_fn",
+  arg_names = ["X", "Y"],
+  input_types = [x_2_array_t, x_2_array_t],
+  body = [syntax.Return()],
   return_type = x_2_array_t,
   type_env = {"X":x_2_array_t})
 
@@ -85,13 +110,21 @@ def map_id(X):
 #  new_fn = tiling_transform.apply(copy=True)
 #  assert isinstance(new_fn, syntax.TypedFn)
 
-def test_lowering():
-  new_fn = lowering.lower(map2_fn, True)
-  print new_fn
+def test_axes():
+  new_fn = lowering.lower(axis_fn, False)
   assert isinstance(new_fn, syntax.TypedFn)
   llvm_fn, parakeet_fn, exec_engine = llvm_backend.compile_fn(new_fn)
   wrapper = run_function.CompiledFn(llvm_fn, parakeet_fn, exec_engine)
-  rslt = wrapper(x2_array, np.array([10,5], dtype=np.int64))
+  rslt = wrapper(x2_array)
+  assert testing_helpers.eq(rslt, x2_array)
+  print rslt
+
+def test_lowering():
+  new_fn = lowering.lower(axis_fn, False)
+  assert isinstance(new_fn, syntax.TypedFn)
+  llvm_fn, parakeet_fn, exec_engine = llvm_backend.compile_fn(new_fn)
+  wrapper = run_function.CompiledFn(llvm_fn, parakeet_fn, exec_engine)
+  rslt = wrapper(x2_array)#, np.array([5,4], dtype=np.int64))
   print rslt
 
 if __name__ == '__main__':
