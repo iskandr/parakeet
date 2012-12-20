de2f00b2d3e7ac74615683ea0637189640bb9f7a
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-12-20 11:20:11 -0500

    updated verify to catch use-before-defined

--- verify.py
+++ verify.py
@@ -1,31 +1,53 @@
 import closure_type
 import core_types
 import syntax
+from syntax import Expr
 import syntax_visitor
+from collect_vars import collect_binding_names
+
 
 class Verify(syntax_visitor.SyntaxVisitor):
   def __init__(self, fn):
+    print fn 
     self.fn = fn
+    self.bound = set(fn.arg_names)
 
   def run(self):
     self.visit_block(self.fn.body)
 
+  def bind_var(self, name):
+    assert name not in self.bound, \
+        "Error: variable %s already bound" % name
+    self.bound.add(name)
+    assert name in self.fn.type_env, \
+        "Assigned variable %s has no entry in type dictionary" % name
 
+  def phi_value_ok(self, lhs_name, v):
+    assert isinstance(v, Expr), \
+       "Invalid expression in phi node: %s" % v     
+    self.visit_expr(v)
+    assert v.type is not None, \
+       "Error in phi node: value %s has no type annotation" % v
+    t = self.fn.type_env[lhs_name]
+    assert v.type == t, \
+       "Invalid type annotation on %v, expected %s but got %s" % (v,t,v.type)
+ 
+  def visit_left_merge(self, phi_nodes):
+    for (k, (v,_)) in phi_nodes.iteritems():
+      self.bind_var(k)
+      self.phi_value_ok(k,v)
+      
   def visit_merge(self, phi_nodes):
     for (k, (left_value, right_value)) in phi_nodes.iteritems():
-      self.visit_expr(left_value)
-      self.visit_expr(right_value)
-      assert isinstance(left_value, syntax.Expr), \
-          "Invalid expression in phi node: %s" % left_value
-      assert isinstance(right_value, syntax.Expr), \
-          "Invalid expression in phi node: %s" % right_value
-      assert left_value.type == right_value.type
-      assert k in self.fn.type_env, "%s not in type env" % k
-      assert self.fn.type_env[k] == left_value.type
+      self.bind_var(k)
+      self.phi_value_ok(k, left_value)
+      self.phi_value_ok(k, right_value)
 
   def visit_Var(self, expr):
+    assert expr.name in self.bound, \
+      "Variable %s used before assignment" % expr.name
     assert expr.name in self.fn.type_env, \
-        "Unknown variable %s" % expr.name
+        "Variable %s has no entry in the type dictionary" % expr.name
 
     assert expr.type == self.fn.type_env[expr.name], \
         "Variable %s in fn %s should have type %s but annotated with %s" % \
@@ -51,6 +73,9 @@ class Verify(syntax_visitor.SyntaxVisitor):
     return verify(fn)
 
   def visit_Assign(self, stmt):
+    lhs_names = collect_binding_names(stmt.lhs)
+    for lhs_name in lhs_names:
+      self.bind_var(lhs_name)
     self.visit_expr(stmt.rhs)
     assert stmt.lhs.type == stmt.rhs.type, \
         "Mismatch between LHS type %s and RHS %s in '%s'" % \
