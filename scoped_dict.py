183ec1f0bc0d3a5d34aa51977a0962a0a010be77
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-12-24 11:10:07 -0500

    added scoped dictionary

---
+++ scoped_dict.py
@@ -0,0 +1,32 @@
+class ScopedDictionary(object):
+  def __init__(self):
+    self.scopes = []
+    
+  def push(self, d = None):
+    if d is None:
+      d = {}
+    self.scopes.append(d)
+  
+  def pop(self):
+    return self.scopes.pop()
+  
+  def top(self):
+    return self.scopes[-1]
+  
+  def get(self, key, default = None):
+    return self.top().get(key, default)
+  
+  def __getitem__(self, key):
+    for scope in reversed(self.scopes):
+      if key in scope:
+        return scope[key]
+    assert False, "Key %s not found" % key
+    
+  def __setitem__(self, key, value):
+    self.top()[key] = value 
+    
+  def __contains__(self, key):
+    for scope in reversed(self.scopes):
+      if key in scope:
+        return True 
+    return False 
