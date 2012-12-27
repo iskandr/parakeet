a3f7f9829650a1b4d327a29d164753d4071841b3
Eric Hielscher <hielscher@gmail.com>
2012-12-27 16:30:46 -0500

    Minor mods.

--- test_rule_30.py
+++ test_rule_30.py
@@ -3,7 +3,7 @@ import numpy as np
 import parakeet
 from testing_helpers import expect, expect_each, run_local_tests
 
-size = 51
+size = 101
 init = np.array(([0] * (size/2)) + [1] + ([0] * (size - size/2 - 1)))
 
 def rule30(idx_vec, old):
@@ -21,12 +21,12 @@ def test_rule30():
   cur = init
   zero_array = np.array([0])
   idx_vecs = np.array([[i-1, i, i+1] for i in range(1,size+1)])
-  print idx_vecs
   for _ in range(100):
     extended = np.concatenate((zero_array, cur, zero_array))
     cur = np.array(map(lambda x: rule30(x, extended), idx_vecs))
     output = np.vstack((output,cur))
-  pylab.matshow(output,cmap=pylab.cm.gray)
+  print output.shape
+  pylab.matshow(output,fignum=100,cmap=pylab.cm.gray)
   pylab.show()
 
 if __name__ == '__main__':
