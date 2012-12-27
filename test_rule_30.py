31f0705e10f6100cb6445957283c7e8da1e37950
Eric Hielscher <hielscher@gmail.com>
2012-12-27 16:57:01 -0500

    Rule 30 mods.

--- test_rule_30.py
+++ test_rule_30.py
@@ -6,7 +6,17 @@ from testing_helpers import expect, expect_each, run_local_tests
 size = 401
 init = np.array(([0] * (size/2)) + [1] + ([0] * (size - size/2 - 1)))
 
+def rule30(idx_vec, extended):
+  a, b, c = extended[idx_vec]
+  if ((a == 1 and b == 0 and c == 0) or
+      (a == 0 and b == 1 and c == 1) or
+      (a == 0 and b == 1 and c == 0) or
+      (a == 0 and b == 0 and c == 1)):
+    return 1
+  else:
+    return 0
 
+use_parakeet = True
 def test_rule30():
   output = init.copy()
   cur = init
@@ -14,7 +24,7 @@ def test_rule30():
   idx_vecs = np.array([[i-1, i, i+1] for i in range(1,size+1)])
   for _ in range(size/2):
     extended = np.concatenate((zero_array, cur, zero_array))
-    def rule30(idx_vec):
+    def rule30_closed(idx_vec):
       a, b, c = extended[idx_vec]
       if ((a == 1 and b == 0 and c == 0) or
           (a == 0 and b == 1 and c == 1) or
@@ -23,12 +33,15 @@ def test_rule30():
         return 1
       else:
         return 0
-    #cur = np.array(map(lambda x: rule30(x, extended), idx_vecs))
-    cur = parakeet.each(rule30, [idx_vecs])
+    if use_parakeet:
+      cur = parakeet.each(rule30_closed, [idx_vecs])
+    else:
+      cur = np.array(map(lambda x: rule30(x, extended), idx_vecs))
     output = np.vstack((output,cur))
-  print output.shape
-  pylab.matshow(output,fignum=100,cmap=pylab.cm.gray)
-  pylab.show()
+
+  if not use_parakeet:
+    pylab.matshow(output,fignum=100,cmap=pylab.cm.gray)
+    pylab.show()
 
 if __name__ == '__main__':
   run_local_tests()
