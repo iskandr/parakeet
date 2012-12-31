4fde465deba03b86d40ef39e305bfda3b3872f50
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-12-31 18:53:25 -0500

    making progress on copy elim

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ print_specialized_function = False
 
 # show lower level typed function before
 # it gets translated to LLVM?
-print_lowered_function = True 
+print_lowered_function = False 
 
 # show the input function to each transformation?
-print_functions_before_transforms = ['CopyElimination']
+print_functions_before_transforms = [] # ['CopyElimination']
 
 # show the function produced by each transformation?
 
-print_functions_after_transforms = ['CopyElimination'] 
+print_functions_after_transforms = [] #['CopyElimination'] 
 
 
 # show functions after tiling transformation?
