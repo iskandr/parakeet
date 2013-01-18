7e58b64b2a954eed9bf72ef75d69455e6f219720
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-01-17 21:23:52 -0500

    added benchmark for col sum

---
+++ test_col_sum.py
@@ -0,0 +1,32 @@
+import numpy as np
+import parakeet
+import testing_helpers
+
+
+
+
+def test_col_sum():
+  m = 5000
+  n = 5000
+  X = np.random.randn(m,n)
+  print "Reduce(Map)"
+  testing_helpers.timed_test(parakeet.sum, [X], np.sum, [X,0], 
+                             assert_faster=True)
+
+def each_col_sum(X):
+  return parakeet.each(parakeet.sum, X)
+
+def test_each_col_sum():
+  m = 5000
+  n = 50000
+  X = np.random.random((m,n))
+  Xt = X.T
+  print "Map(Reduce)"
+  # summing rows of Xt is same as summing cols of original 
+  testing_helpers.timed_test(each_col_sum, [Xt], np.sum, [Xt,1], 
+                             assert_faster=True)
+
+
+if __name__ == '__main__':
+  testing_helpers.run_local_tests()
+
