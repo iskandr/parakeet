a42a9eddeb9e651f890774a4397c75b281af4b84
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-02-11 13:56:18 -0500

    added a list comp test that breaks current ast code

--- tests/test_list_comp.py
+++ tests/test_list_comp.py
@@ -1,13 +1,22 @@
 import numpy as np 
 import testing_helpers 
 
+from testing_helpers import expect
+
+x = np.array([1,2,3,4,5])
 
 def identity_comprehension(x):
   return [xi for xi in x]
 
 def test_identity_comprehension():
-  x = np.array([1,2,3,4,5])
-  testing_helpers.expect(identity_comprehension, [x], x)
+  expect(identity_comprehension, [x], x)
+
+
+def sqr_elts(x):
+  return [xi**2 for xi in x]
+
+def test_sqr_elts():
+  expect(sqr_elts, [x], x**2)
   
 if __name__ == '__main__':
   testing_helpers.run_local_tests()
