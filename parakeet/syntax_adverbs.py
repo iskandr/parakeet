d45c871dcfe5af30776f3433bea4870891d6cabe
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-05-25 22:09:43 -0500

    split up syntax module into smaller parts

---
+++ parakeet/syntax_adverbs.py
@@ -0,0 +1,159 @@
+from syntax_expr import Expr
+ 
+class Adverb(Expr):
+  _members = ['fn']
+  
+class IndexAdverb(Adverb):
+  _members = ['shape']
+
+
+class IndexMap(IndexAdverb):
+  """
+  Map from each distinct index in the shape to a value 
+  """
+  pass 
+  
+class IndexReduce(IndexAdverb):
+  """
+  Expect the 'fn' field to take indices and produces 
+  element values, whereas 'combine' takes pairs of element 
+  values and combines them. 
+  """
+  _members = ['combine', 'init']
+  
+
+class DataAdverb(Adverb):
+  _members = ['args', 'axis']
+
+  def fn_to_str(self, fn):
+    if hasattr(fn, 'name'):
+      return fn.name
+    else:
+      return str(fn)
+
+  def args_to_str(self):
+    if isinstance(self.args, (list, tuple)):
+      return ", ".join([str(arg) + ":" + str(arg.type) for arg in self.args])
+    else:
+      return str(self.args)
+
+  def __repr__(self):
+    s = "%s(axis = %s, args = (%s), type=%s, fn = %s)" % \
+          (self.node_type(), self.axis,
+           self.args_to_str(),
+           self.type,
+           self.fn_to_str(self.fn),)
+    return s
+
+  def __str__(self):
+    return repr(self)
+
+class Map(DataAdverb):
+  pass
+
+class AllPairs(DataAdverb):
+  pass 
+
+class Accumulative(DataAdverb):
+  """
+  Adverbs such as Reduce and Scan which carry an accumulated value and require a
+  'combine' function to merge the accumulators resulting from parallel
+  sub-computations.
+  """
+  _members = ['combine', 'init']
+
+
+  def node_init(self):
+    # assert self.init is not None
+    # assert self.combine is not None
+    pass
+
+class Reduce(Accumulative):
+  def __repr__(self):
+    return "Reduce(axis = %s, args = (%s), type = %s, init = %s, map_fn = %s, combine = %s)" % \
+        (self.axis,
+         self.args_to_str(),
+         self.type,
+         self.init,
+         self.fn_to_str(self.fn),
+         self.fn_to_str(self.combine))
+
+class Scan(Accumulative):
+  _members = ['emit']
+
+  def __repr__(self):
+    s = "%s(axis = %s, args = {%s}, type = %s, " \
+        % (self.node_type(), self.axis,
+           self.args_to_str(),
+           self.type
+          )
+    s += "init = %s, map_fn = %s, combine = %s, emit = %s)" % \
+         (self.init,
+          self.fn_to_str(self.fn),
+          self.fn_to_str(self.combine),
+          self.fn_to_str(self.emit))
+    return s
+
+
+class Tiled(object):
+  _members = ['axes', 'fixed_tile_size']
+
+  def __repr__(self):
+    s = "%s(axes = %s, args = (%s), type=%s, fn = %s)" % \
+          (self.node_type(), self.axes,
+           self.args_to_str(),
+           self.type,
+           self.fn_to_str(self.fn),)
+    return s
+
+class TiledMap(Tiled, Map):
+  pass
+
+class TiledAllPairs(Tiled, AllPairs):
+  pass
+
+class TiledReduce(Tiled, Reduce):
+  def __repr__(self):
+    s = ("%s(axes = %s, args = {%s}, type = %s, init = %s, map_fn = %s, combine = %s)") % \
+        (self.node_type(), self.axes,
+         self.args_to_str(),
+         self.type,
+         self.init,
+         self.fn_to_str(self.fn),
+         self.fn_to_str(self.combine),
+        )
+    return s
+
+class TiledScan(Tiled, Scan):
+  def __repr__(self):
+    s = "%s(axes = %s, args = {%s}, type = %s, " \
+        % (self.node_type(), self.axes,
+           self.args_to_str(),
+           self.type
+          )
+    s += "init = %s, map_fn = %s, combine = %s, emit = %s)" % \
+         (self.init,
+          self.fn_to_str(self.fn),
+          self.fn_to_str(self.combine),
+          self.fn_to_str(self.emit))
+    return s
+
+  
+class Conv(Adverb):
+  _members = ['x', 'window_shape']
+  
+  def __repr__(self):
+    return "Conv(fn = %s, x = %s, window_shape=%s)" % \
+      (self.fn, self.x, self.window_shape)
+      
+  def __str__(self):
+      return repr(self)
+
+class ConvBorderFn(Conv):
+  _members = ['border_fn']
+  
+class ConvBorderValue(Conv):
+  _members = ['border_value']
+  
+class ConvPadding(Conv):
+  _members = ['fill_value']
---
+++ parakeet/syntax_stmts.py
@@ -0,0 +1,118 @@
+
+from  node import Node
+
+
+class Stmt(Node):
+  pass
+
+def block_to_str(stmts):
+  body_str = '\n'
+  body_str += '\n'.join([str(stmt) for stmt in stmts])
+
+  return body_str.replace('\n', '\n    ')
+
+def phi_nodes_to_str(phi_nodes):
+  parts = ["%s <- phi(%s, %s)" %
+           (var, left, right) for (var, (left, right)) in phi_nodes.items()]
+  whole = "\n" + "\n".join(parts)
+  # add tabs
+  return whole.replace("\n", "\n    ")
+
+class Assign(Stmt):
+  _members = ['lhs', 'rhs']
+
+  def __str__(self):
+    if hasattr(self.lhs, 'type') and self.lhs.type:
+      return "%s : %s = %s" % (self.lhs, self.lhs.type, self.rhs)
+    else:
+      return "%s = %s" % (self.lhs, self.rhs)
+
+class ExprStmt(Stmt):
+  """Run an expression without binding any new variables"""
+
+  _members = ['value']
+
+  def __str__(self):
+    assert self.value is not None
+    return "ExprStmt(%s)" % self.value
+
+class Comment(Stmt):
+  _members = ['text']
+
+  def __str__(self):
+    s = "#"
+    for (i, c) in enumerate(self.text):
+      if i % 78 == 0:
+        s += "\n# "
+      s += c
+    s += "\n#"
+    return s
+
+
+class Return(Stmt):
+  _members = ['value']
+
+  def __str__(self):
+    return "Return %s" % self.value
+
+class If(Stmt):
+  _members = ['cond', 'true', 'false', 'merge']
+
+  def __str__(self):
+    s = "if %s:" % self.cond
+    if (len(self.true) + len(self.false)) > 0:
+      s += "%s\n" % block_to_str(self.true)
+    else:
+      s += "\n"
+    if len(self.false) > 0:
+      s += "else:%s\n" % block_to_str(self.false)
+    if len(self.merge) > 0:
+      s += "(merge-if)%s" % phi_nodes_to_str(self.merge)
+    return s
+
+  def __repr__(self):
+    return str(self)
+
+class While(Stmt):
+  """
+  A loop consists of a header, which runs before each iteration, a condition for
+  continuing, the body of the loop, and optionally (if we're in SSA) merge nodes
+  for incoming and outgoing variables of the form
+  [(new_var1, (old_var1,old_var2)]
+  """
+
+  _members = ['cond', 'body', 'merge']
+
+  def __repr__(self):
+    s = "while %s:\n  "  % self.cond
+    if len(self.merge) > 0:
+      s += "(header)%s\n  " % phi_nodes_to_str(self.merge)
+    if len(self.body) > 0:
+      s +=  "(body)%s" % block_to_str(self.body)
+    return s
+
+  def __str__(self):
+    return repr(self)
+
+class ForLoop(Stmt):
+  """
+  Having only one loop construct started to become cumbersome, especially now
+  that we're playing with loop optimizations.
+
+  So, here we have the stately and ancient for loop.  All hail its glory.
+  """
+
+  _members = ['var', 'start', 'stop', 'step', 'body', 'merge']
+  
+    
+  def __str__(self):
+    s = "for %s in range(%s, %s, %s):" % \
+      (self.var,
+       self.start.short_str(),
+       self.stop.short_str(),
+       self.step.short_str())
+
+    if self.merge and len(self.merge) > 0:
+      s += "\n  (header)%s\n  (body)" % phi_nodes_to_str(self.merge)
+    s += block_to_str(self.body)
+    return s
