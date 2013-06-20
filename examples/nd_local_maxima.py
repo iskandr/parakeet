dabd4c38ceb51c3051342fd0c0efcbf98cffa15e
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-06-20 16:04:24 -0400

    working on nd operations

---
+++ examples/nd_local_maxima.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import parakeet 
+
+def wrap(pos, offset, bound):
+    return ( pos + offset ) % bound
+
+def edge(pos, offset, bound):
+    return min(bound-1,max(0,pos+offset))
+
+def reflect(pos, offset, bound):
+    idx = pos+offset
+    return min(2*(bound-1)-idx,max(idx,-idx))
+  
+@parakeet.jit 
+def local_maxima(data, wsize, mode=wrap):
+  def is_max(position):
+    myval = data[position]
+    def is_bigger_neighbor(offset):
+      neighbor_idx = tuple(mode(p,o-w/2,w) for p,o,w in zip(position, offset, wsize))
+      return data[neighbor_idx] >= myval
+    return not parakeet.any(parakeet.imap(wsize, is_bigger_neighbor))
+  return parakeet.imap(data.shape, is_max)
+
+
+if __name__  == '__main__':
+  shape = (10,20,30,4)
+  x = np.random.randn(*shape)
+  wsize = (3,5,3,1)
+  y = local_maxima(x, wsize)
