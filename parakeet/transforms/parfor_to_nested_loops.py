d286a806c47ba375cf06b24999f1352706e5dc46
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-07-23 18:32:17 -0400

    added parfor_to_nested_loops

---
+++ parakeet/transforms/parfor_to_nested_loops.py
@@ -0,0 +1,7 @@
+from transform import Transform
+
+class ParForToNestedLoops(Transform):
+  def transform_ParFor(self, stmt):
+    fn = self.transform_expr(stmt.fn)
+    self.nested_loops(stmt.bounds, fn)
+    
