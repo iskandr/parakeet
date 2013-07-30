b1ac3951a804e93b6fc3093880982bcd008d0d0b
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-07-30 17:47:22 -0400

    started C backend

---
+++ parakeet/c_backend/compiler.py
@@ -0,0 +1,98 @@
+from treelike import NestedBlocks
+
+from .. import names 
+from ..analysis import SyntaxVisitor
+from ..syntax import Var
+from ..ndtypes import (Int8, Int16, Int32, Int64, Float32, Float64, 
+                       TupleT, ScalarT, ArrayT, 
+                       elt_type) 
+
+class Compiler(SyntaxVisitor):
+  
+  def __init__(self):
+    self.blocks = []
+    self.name_versions = {}
+    self.name_mappings = {}
+  
+  def push(self):
+    self.blocks.append([])
+  
+  def pop(self):
+    stmts = self.blocks.pop()
+    return "\n".join("  " + stmt for stmt in stmts)
+  
+  def append(self, stmt):
+    self.blocks[0].append(stmt)
+  
+  def stmt(self, s):
+    self.append("%s;" % s)
+  
+  def fresh_name(self, prefix):
+    version = self.name_versions.get(prefix, 1)
+    if version == 1:
+      return prefix 
+    else:
+      return "%s_%d" % (prefix, version)
+  
+  def name(self, ssa_name):
+    """
+    Convert from ssa names, which might have large version numbers and contain 
+    syntactically invalid characters to valid local C names
+    """
+    if ssa_name in self.name_mappings:
+      return self.name_mappings[ssa_name]
+    prefix = names.original(ssa_name)
+    prefix = prefix.replace(".", "_")
+    name = self.fresh_name(prefix)
+    self.name_mappings[ssa_name] = name 
+    return name 
+  
+  def dtype(self, t):
+    if t == Int8: return "NPY_INT8"
+    elif t == Int16: return "NPY_INT16"
+    elif t == Int32: return "NPY_INT32"
+    elif t == Int64: return "NPY_INT64"
+    elif t == Float32: return "NPY_FLOAT32"
+    elif t == Float64: return "NPY_FLOAT64"
+    else:
+      assert False, "Unsupported element type %s" % t
+  
+  def ctype(self, t):
+    if t == Int8: return "int8_t"
+    elif t == Int16: return "int16_t"
+    elif t == Int32: return "int32_t"
+    elif t == Int64: return "int64_t"
+    elif t == Float32: return "float32_t"
+    elif t == Float64: return "float64_t"
+    elif not isinstance(t, ScalarT):
+      return "PyObject*"
+    else:
+      assert False, "Unsupported type %s" % t 
+    
+  
+  def tuple_to_stack_array(self, expr):
+    assert expr.type.__class__ is TupleT 
+    t0 = expr.type.elt_types
+    assert all(t == t0 for t in expr.type.elt_types[1:])
+    array_name = self.fresh_name("array_from_tuple")
+    n = len(expr.type.elt_types)
+    self.stmt("%s %s[%d]" % (self.ctype(t0), array_name, n))
+    
+  def visit_AllocArray(self, expr):
+    shape = self.tuple_to_stack_array(expr.shape)
+    t = self.dtype(elt_type(expr.type))
+    return "PyArray_SimpleNew(%d, %s, %s)" % (expr.type.rank, shape, t)
+  
+  def visit_Const(self, expr):
+    return "%s" % expr.value 
+  
+  def visit_Var(self, expr):
+    return self.name(expr.name)
+  
+  def visit_Assign(self, stmt):
+    assert stmt.lhs.__class__ is Var
+    lhs_name = self.name(stmt.lhs.name)
+    rhs = self.visit_expr(stmt.rhs)
+    self.stmt("%s = %s" % (lhs_name, rhs))
+    
+  #def visit_PrimCall(self, expr):
