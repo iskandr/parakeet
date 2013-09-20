20a2310b6256c76c58fd935a058d4d5c560ac174
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-09-19 22:38:40 -0400

    reorganized c backend config

--- parakeet/c_backend/config.py
+++ parakeet/c_backend/config.py
@@ -1,10 +1,23 @@
-debug = False
-check_pyobj_types = True
+##########################
+#  Performance Options   #
+##########################
 fast_math = True
-pure_c = True
 use_openmp = False 
 
+##########################
+# Insert Debugging Code  #
+##########################
+debug = False
+check_pyobj_types = False
+
+
+#########################
+#  Verbose Printing     #
+#########################
 print_input_ir = False
-print_commands = True
-print_function_source = True
+print_commands = False
+print_function_source = False
 print_module_source = False
+
+# Generate a .c file or a .cpp? 
+pure_c = True
