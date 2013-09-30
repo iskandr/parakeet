43009960ef838d1dc1693885511e2db17b6ed5a3
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-09-30 12:31:29 -0400

    setting up travis CI

--- parakeet/builder/builder.py
+++ parakeet/builder/builder.py
@@ -8,8 +8,6 @@ from arith_builder import ArithBuilder
 from array_builder import ArrayBuilder
 from loop_builder import LoopBuilder 
 from call_builder import CallBuilder
-from numba import type_inference
-
 
 class Builder(ArithBuilder, ArrayBuilder, CallBuilder, LoopBuilder):
   
