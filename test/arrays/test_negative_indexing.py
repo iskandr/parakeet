47f60c87c9206ef6cee2998eb42eec992dd432aa
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-10-02 14:33:30 -0400

    added more negative indexing tests

--- test/arrays/test_negative_indexing.py
+++ test/arrays/test_negative_indexing.py
@@ -13,10 +13,39 @@ def test_negative_stop_1d():
     expect(negative_stop_2d, [mat], mat[:-2])
 
 def negative_stop_2d(x):
-    return x[:-2, :-2]
+    return x[:-2, :-3]
 
 def test_negative_stop_2d():
-    expect(negative_stop_2d, [mat], mat[:-2,:-2])
+    expect(negative_stop_2d, [mat], mat[:-2,:-3])
+
+
+def negative_start_1d(x):
+    return x[-2:]
+
+def test_negative_start_1d():
+    expect(negative_start_1d, [vec], vec[-2:])
+    expect(negative_start_2d, [mat], mat[-2:])
+
+
+def negative_start_2d(x):
+    return x[-2:, -3:]
+
+def test_negative_start_2d():
+    expect(negative_start_2d, [mat], mat[-2:, -3:])
+
+
+def negative_step_1d(x):
+    return x[::-2]
+
+def test_negative_step_1d():
+    expect(negative_step_1d, [vec], vec[::-2])
+    expect(negative_step_2d, [mat], mat[::-2])
+
+def negative_step_2d(x):
+    return x[::-2, ::-3]
+
+def test_negative_step_2d():
+    expect(negative_step_2d, [vec], mat[::-2, ::-3])
 
 if __name__ == "__main__":
     run_local_tests()
