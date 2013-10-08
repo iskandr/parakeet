7bd3cec8fe11d5768ed85e0c87d57c2af5c4a4ec
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-10-08 16:19:22 -0400

    added Julia fractal as benchmark

---
+++ benchmarks/julia.py
@@ -0,0 +1,34 @@
+import numpy as np 
+from parakeet import testing_helpers
+
+def kernel(zr, zi, cr, ci, lim, cutoff):
+    ''' Computes the number of iterations `n` such that 
+        |z_n| > `lim`, where `z_n = z_{n-1}**2 + c`.
+    '''
+    count = 0
+    while ((zr*zr + zi*zi) < (lim*lim)) and count < cutoff:
+        zr, zi = zr * zr - zi * zi + cr, 2 * zr * zi + ci
+        count += 1
+    return count
+
+def julia_loops(cr, ci, N, bound=1.5, lim=1000., cutoff=1e6):
+    ''' Pure Python calculation of the Julia set for a given `c`.  No NumPy
+        array operations are used.
+    '''
+    julia = np.empty((N, N), dtype=np.uint32)
+    grid_x = np.linspace(-bound, bound, N)
+    for i, x in enumerate(grid_x):
+        for j, y in enumerate(grid_x):
+            julia[i,j] = kernel(x, y, cr, ci, lim, cutoff=cutoff)
+    return julia
+
+from timer import compare_perf 
+cr=0.285
+ci=0.01
+N=100
+bound = 1.5 
+lim = 1000
+cutoff = 1e6 
+
+compare_perf(julia_loops, [cr, ci, N, bound, lim, cutoff])
+
