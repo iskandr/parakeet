6717792597e11502a64ccaba8233f4da1561a5a2
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-10-10 17:30:32 -0400

    version bump

--- parakeet/version.py
+++ parakeet/version.py
@@ -2,6 +2,6 @@ __author__  = 'Alex Rubinsteyn'
 __email__   = 'alex -dot- rubinsteyn -at- gmail -dot- com'
 __desc__    = 'Runtime compiler for numerical Python'
 __license__     = 'BSD3'
-__version__     = '0.17'
+__version__     = '0.17.1'
 __website__     = 'http://www.parakeetpython.com' 
 
