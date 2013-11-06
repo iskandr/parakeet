d1665dcc6dd4d461562123062c5272b936550ca8
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-11-06 17:22:24 -0500

    debugging vector dot

--- benchmarks/matmult.py
+++ benchmarks/matmult.py
@@ -11,7 +11,7 @@ def matmult_high_level(X,Y):
 def matmult_loops(X,Y,Z):
   m, d = X.shape
   n = Y.shape[1]
-  for i in xrange(m):
+  for y in xrange(m):
     for j in xrange(n):
       total = X[i,0] * Y[0,j] 
       for k in xrange(1,d):
@@ -19,11 +19,11 @@ def matmult_loops(X,Y,Z):
       Z[i,j] = total 
   return Z
 
-n, d = 800, 800 
-m = 800
-X = np.random.randn(m,d).astype('float64')
-Y = np.random.randn(d,n).astype('float64')
-Z = np.zeros((m,n)).astype('float64')
+n, d = 1200, 1200 
+m = 1200
+X = np.random.randn(m,d).astype('float32')
+Y = np.random.randn(d,n).astype('float32')
+Z = np.zeros((m,n)).astype('float32')
 from compare_perf import compare_perf
 
 compare_perf(matmult_high_level, [X,Y],cpython=True, numba=False,extra = {'numpy':np.dot}, suppress_output = False)
--- test/algorithms/test_matmult_comprehensions.py
+++ test/algorithms/test_matmult_comprehensions.py
@@ -3,25 +3,41 @@ import numpy as np
 from parakeet import jit 
 from parakeet.testing_helpers import run_local_tests, expect
 
-int_mat = np.reshape(np.arange(6), (2,3))
-float_mat = np.sqrt(int_mat)
-bool_mat = int_mat % 2
+int64_mat = np.reshape(np.arange(6), (2,3)).astype('int64')
+int32_mat = int64_mat.astype('int32')
 
-matrices = [int_mat, float_mat, bool_mat]
-      
-@jit 
-def dot(x,y):
-  return sum(x*y)
+float64_mat = int64_mat.astype('float64')
+float32_mat = int64_mat.astype('float32') 
+bool_mat = int64_mat % 2
 
-@jit 
+matrices = [int64_mat, int32_mat, float64_mat, float32_mat, bool_mat]
+
 def matmult_comprehensions(X,Y):
-  return np.array([[dot(x,y) for y in Y.T] for x in X])
+  return np.array([[np.dot(x,y) for y in Y.T] for x in X])
 
-def test_matmult_comprehensions():
-  for X in matrices:
-    for Y in matrices:
-      res = np.dot(X, Y.T)
-      expect(matmult_comprehensions, [X,Y.T], res)
+def test_matmult_int64_int32():
+  expect(matmult_comprehensions, [int64_mat, int32_mat.T], np.dot(int64_mat, int32_mat.T))
+
+def test_matmult_int32_int32():
+  expect(matmult_comprehensions, [int32_mat, int32_mat.T], np.dot(int32_mat, int32_mat.T))
+
+def test_matmult_float64_float32():
+  expect(matmult_comprehensions, [float64_mat, float32_mat.T], np.dot(float64_mat, float32_mat.T))
+
+def test_matmult_float32_float32():
+  expect(matmult_comprehensions, [float32_mat, float32_mat.T], np.dot(float32_mat, float32_mat.T))
+
+def test_matmult_float32_int32():
+  expect(matmult_comprehensions, [float32_mat, int32_mat.T], np.dot(float32_mat, int32_mat.T))
+
+def test_matmult_float64_bool():
+  expect(matmult_comprehensions, [float64_mat, bool_mat.T], np.dot(float64_mat, bool_mat.T))
+
+def test_matmult_bool_bool():
+  expect(matmult_comprehensions, [bool_mat, bool_mat.T], np.dot(bool_mat, bool_mat.T))
+
+def test_matmult_bool_int32():
+  expect(matmult_comprehensions, [bool_mat, int32_mat.T], np.dot(bool_mat, int32_mat.T))
       
 if __name__ == "__main__":
   run_local_tests()
