af75d95e5defe8bfd95144c7f790afb5e6971f12
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-11-21 16:43:29 -0500

    changing stride specialization into value specialization

--- parakeet/value_specialization/value_specialization.py
+++ parakeet/value_specialization/value_specialization.py
@@ -1,6 +1,7 @@
 import numpy as np 
 
 
+from .. import syntax 
 from .. syntax.helpers import const 
 from ..transforms  import Transform, Simplify, Phase, DCE 
 
@@ -22,6 +23,27 @@ class ValueSpecializer(Transform):
     env, _ = symbolic_call(fn, self.abstract_inputs)
     self.env = env 
   
+  def lookup_expr(self, expr):
+    c = expr.__class__ 
+    if c is syntax.Var:
+      return self.env[expr.name]
+    else:
+      return unknown 
+    
+  def lookup_expr_list(self, exprs):
+    return [self.lookup_expr(e) for e in exprs]
+  
+  def transform_ParFor(self, stmt):
+    fn = self.get_fn(stmt.fn)
+    closure_elts = self.closure_elts(stmt.fn)
+    abstract_values = self.lookup_expr_list(closure_elts)
+    # all the index arguments have to be marked unknown 
+    for _ in xrange(len(abstract_values), len(fn.input_types)):
+      abstract_values.append(unknown)
+    new_fn = specialize_abstract_values(fn, tuple(abstract_values))
+    stmt.fn = self.closure(new_fn, closure_elts)
+    return stmt 
+    
   def transform_Var(self, expr):
     if expr.name in self.env:
       abstract_value = self.env[expr.name]
@@ -34,17 +56,18 @@ class ValueSpecializer(Transform):
   
 def has_unit_stride(abstract_value):
   c = abstract_value.__class__
-  if c is Array:
+  
+  if c is Const:
+    return abstract_value.value == 1
+  elif c is Array:
     return has_unit_stride(abstract_value.strides)
+  elif c is Tuple:
+    return any(has_unit_stride(elt) 
+               for elt in abstract_value.elts)
   elif c is Struct:
     return any(has_unit_stride(field_val) 
                for field_val 
                in abstract_value.fields.itervalues())
-  elif c is Tuple:
-    return any(has_unit_stride(elt) 
-               for elt in abstract_value.elts)
-  elif c is Const:
-    return abstract_value.value == 1
   else:
     return False
 
@@ -70,12 +93,10 @@ def from_python_list(python_values):
   return tuple([from_python(v) for v in python_values]) 
 
 _cache = {}
-def specialize(fn, python_values):
-  abstract_values = from_python_list(python_values)
+def specialize_abstract_values(fn, abstract_values):
   key = (fn.cache_key, abstract_values)
   if key in _cache:
     return _cache[key]
-
   if any(has_unit_stride(v) for v in abstract_values):
     specializer = ValueSpecializer(abstract_values)
     transforms = Phase([specializer, Simplify, DCE],
@@ -88,3 +109,8 @@ def specialize(fn, python_values):
     new_fn = fn
   _cache[key] = new_fn
   return new_fn
+
+
+def specialize(fn, python_values):
+  return specialize_abstract_values(fn,  from_python_list(python_values))
+  
