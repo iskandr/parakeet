02e2c9d1d07084551dd275baf908a398d7013da6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-11-21 23:19:33 -0500

    minor version bump

--- parakeet/package_info.py
+++ parakeet/package_info.py
@@ -2,6 +2,6 @@ __author__  = 'Alex Rubinsteyn'
 __email__   = 'alex -dot- rubinsteyn -at- gmail -dot- com'
 __desc__    = 'Runtime compiler for numerical Python'
 __license__     = 'BSD3'
-__version__     = '0.23'
+__version__     = '0.23.1'
 __website__     = 'http://www.parakeetpython.com' 
 
